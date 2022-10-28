893ef52acfc4aa9f00e6c5aaf1141462552828c2
khorshed-alam-kbd <khorshedalam.kbd@gamil.com>
2022-10-28 17:40:41 +0600

    platinam-tutorial-server upgrade

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const express = require('express')
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const courses = require('./data/courses.json')
+
+app.get('/', (req, res) => {
+    res.send('NEWS API Running')
+});
